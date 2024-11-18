2b9899cea9019ab0fd22ae4ccba4e6628458a011
Luca Kursawe <lucakursawe@gmail.com>
2024-11-18 19:17:23 +0100

    project page added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 const projects = [
+<<<<<<< HEAD
     { id: 1, name: 'Project One', description: 'Description for project one' },
     { id: 2, name: 'Project Two', description: 'Description for project two' },
     { id: 3, name: 'Project Three', description: 'Description for project three' },
+=======
+    { id: 1, name: 'REP.js', description: 'A web application for gym enthusiasts to calculate and track their Estimated One-Rep Max (E1RM). Designed for auto-regulating and optimizing training, it features profile management and interactive data visualization.' },
+>>>>>>> 242dd96 (project page added)
 ];
 
 const ProjectsPage: React.FC = () => {
     return (
         <div>
+<<<<<<< HEAD
             <h1>Projects</h1>
+=======
+            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+                Projects
+            </h1>
+>>>>>>> 242dd96 (project page added)
             <ul>
                 {projects.map((project) => (
                     <li key={project.id}>
